81f01ddb0b0df88c801e2eddbf5d6a312903c6fe
Max Petersen <mglxp5@gmail.com>
2018-09-02 22:29:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  sentence = ""
+  if array.size == 1
+    sentence = array.first
+  elsif array.size == 2
+    sentence = array[0] + " and " + array[1]
+  else
+  
+    lastWord = array.pop
+  
+    sentence = array.join(", ")
+  
+    sentence = sentence + ", and " + lastWord
+  end
+  return sentence
 end
+
+
+#oxford_comma([hi, hello, greetings])
