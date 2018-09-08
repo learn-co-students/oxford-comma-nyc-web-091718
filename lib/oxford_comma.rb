3a9aa5c177e79d51d158a917bddba76fed6d7300
DDarrow123 <ddarrowphl@gmail.com>
2018-09-08 08:57:05 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+  if array.length == 2
+    str = array.join(' and ')
+
+  elsif array.length > 2
+    last_item = array.pop
+    str = array.join(', ')
+    str << ', and ' << last_item
+  else
+    return array.pop
+  end
+  str
+
 end
