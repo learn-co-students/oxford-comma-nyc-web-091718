222451ac9ccd6ae6925ac9fa37312e420d56e42d
jtang93 <tang.jonathan93@gmail.com>
2018-09-13 01:19:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size <= 2
+    array.join(" and ")
+  else
+    last_element = "and #{array[-1]}"
+    array[-1].replace(last_element)
+    array.join(", ")
+  end
 end
